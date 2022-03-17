d140ec15dfc203b8635269eb47c15c4c402867ff
Karthikeyan K <80576697+triplek24@users.noreply.github.com>
2022-03-17 12:20:46 +0530

    Update 3_day.js

--- 3_day.js
+++ 3_day.js
@@ -4,7 +4,7 @@ var numbers = [ 10, 20, 30, 40, 50]
 for (var i=0; i < numbers.length; i++) {
    console.log(numbers[i])        
 } 
-//using object property                               //10, 20, 30, 40, 50
+//using object property                               //output: 10, 20, 30, 40, 50
 var person = {
     fname: "Nick",
     lname: "Jonas",
@@ -12,14 +12,15 @@ var person = {
  }; 
  for (let x in person) {              //For_In_Loop
     console.log(x + ": "+ person[x])
- }// fname nick lname Jonas age 26
+ }                                                    //output: fname nick lname Jonas age 26
 
  //using FOR_Of loop
  const i = 'boo';
 
 for (const value of i) {
   console.log(value);
-}
+} 
+//output
 // "b"
 // "o"
 // "o"
